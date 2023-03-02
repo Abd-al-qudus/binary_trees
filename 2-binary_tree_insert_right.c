2435444289a31f85969a8312ddbc09cd634c5945
Abd-al-qudus <jimohqudus86@gmail.com>
2023-03-02 14:23:02 +0100

    corrected 2-right add BT

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -5,28 +5,23 @@
  * @value: value to store
  * Return: a pointer to the new node
  */
-
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node, *ptr;
+	binary_tree_t *new;
 
-	node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-	if (node == NULL)
+	if (!parent)
 		return (NULL);
-	if (parent == NULL)
+
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
 		return (NULL);
-	node->n = value;
-	node->parent = parent;
-	node->left = NULL;
-	node->right = NULL;
-	if (parent->right != NULL)
-	{
-		ptr = parent->right;
-		parent->right = node;
-		ptr->parent = node;
-		node->right = ptr;
-	}
-	else
-		parent->right = node;
-	return (node);
+
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = parent->right;
+	parent->right = new;
+	if (new->right)
+		new->right->parent = new;
+	return (new);
 }
